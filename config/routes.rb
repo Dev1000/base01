549f8becb90fb851a1f6934df405eebc2cb5b413
lalo <lalo@lalo.io>
2022-03-04 15:35:54 -0500

    Add Hello World to root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   end
   post "/graphql", to: "graphql#execute"
   get 'hello_world', to: 'hello_world#index'
+
+  root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
